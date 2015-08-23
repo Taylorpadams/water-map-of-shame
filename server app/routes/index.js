614c586d4659bacbf914d8199ba7887622b0fae3
Michael Hsu <pig0826usanj@gmail.com>
2015-08-22 18:35:45 -0700

    refresh page after publish

--- server app/routes/index.js
+++ server app/routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-
 router.get('/', function(req, res) {
+  res.sendFile('index.html');
+});
+
+
+router.post('/publish/:id', function(req, res) {
+  var featureIdToPublish = req.param("id");
+  res.sendStatus(200);
+});
+
+router.post('/unpublish/:id', function(req, res) {
+    var featureIdToUnPublish = req.param("id");
   res.sendFile('index.html');
 });
 
