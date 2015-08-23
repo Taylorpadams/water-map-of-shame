1c57fd1c860dc6b4f5c111f08d7564a7270f82f4
Michael Hsu <pig0826usanj@gmail.com>
2015-08-23 14:13:26 -0700

    image upload

--- ionic-app/www/js/app.js
+++ ionic-app/www/js/app.js
@@ -8,7 +8,7 @@
 angular.module('starter', ['ionic', 'starter.services', 'ngCordova'])
 
   .config(function ($compileProvider, $stateProvider, $httpProvider, $urlRouterProvider) {
-    $compileProvider.imgSrcSanitizationWhitelist(/^\s*(https?|ftp|mailto|file|tel):/);
+    $compileProvider.imgSrcSanitizationWhitelist(/^\s*(https?|ftp|mailto|file|tel|data):/);
     $httpProvider.defaults.headers.common['Access-Control-Allow-Headers'] = '*';
   })
 
@@ -42,9 +42,22 @@ angular.module('starter', ['ionic', 'starter.services', 'ngCordova'])
     // Called when the form is submitted
     $scope.createReport = function (report) {
 
+     
+
+      // window.resolveLocalFileSystemURI(report.imageData, function(fileEntry) {
+      //       alert("file entry: "+JSON.stringify(fileEntry));
+      //       alert("filed entry url: "+fileEntry.toURL());
+      //       fileEntry.file(function(fileObj) {
+      //         alert("file obj: "+JSON.stringify(fileObj));
+               
+      //           //now use the fileName in your method
+      //           //ft.upload(fileName ,serverURL + '/ajax.php?fname=appuploadspotimage'...);
+
+      //       });
+      //   });
       var lat = report.latlong.substring(0, report.latlong.indexOf(","));
       var long = report.latlong.substring(report.latlong.indexOf(",") + 1);
-
+      //var imageDataUrl = encodeImageUri($scope.report.imageData);
       var feature = {
         "geometry": { "x": long, "y": lat },
         "attributes": {
@@ -52,33 +65,82 @@ angular.module('starter', ['ionic', 'starter.services', 'ngCordova'])
           category: report.category,
           description: report.description,
           date: report.date,
+          imageurl: "http://www.cityofhenderson.com/images/default-source/utility-services/water_waste.jpg?sfvrsn=2",
           isPublished: 0
         }
       };
+
       var features = [];
       features.push(feature);
-      //console.log(features);
-      // Simple POST request example (passing data) :
+      console.log(features);
+      //Simple POST request example (passing data) :
       $http.post('http://services6.arcgis.com/IjlJ3SNhNad1djSr/arcgis/rest/services/Water_Wastage/FeatureServer/0/addFeatures?features=' + encodeURIComponent(JSON.stringify(features)) + '&f=json', null).
         then(function (response) {
           // this callback will be called asynchronously
           // when the response is available
-          console.log(response);
+          console.log(JSON.stringify(response));
+          var objectId = response.data.addResults[0].objectId;
           
-          // An alert dialog
-
-            var alertPopup = $ionicPopup.alert({
+             var alertPopup = $ionicPopup.alert({
               title: 'Submittied Complaint',
-              template: 'Complaint Submitted!'
+              template: "Submitted!!!"
             });
             alertPopup.then(function (res) {
-              console.log('Submitted');
-              $state.go('');
+              //$state.report = {};
             });
-          
-          
-          
+           var uri = 'http://services6.arcgis.com/IjlJ3SNhNad1djSr/arcgis/rest/services/Water_Wastage/FeatureServer/0/' + objectId + '/addAttachment&f=json';
+    //      var fd = new FormData();
+   // var blob = b64toBlob(report.imageData.replace("data:image/jpeg;base64,", ""), 'image/jpg');
+   // fd.append("attachment", blob, "userImage.jpg");
+    //      $http.post(uri, fd, {
+    //         transformRequest: angular.identity,
+    //         headers: {'Content-Type': undefined}
+    //     })
+    //     .then(function(response){
+    //       alert(JSON.stringify(response));
+    //     }, function(response){
+    //         alert(JSON.stringify(response));
+    //     });
+     
+        
+          // function win(r) {
+          //   console.log("Code = " + r.responseCode);
+          //   console.log("Response = " + r.response);
+          //   console.log("Sent = " + r.bytesSent);
+            
+         
 
+          // }
+
+        //   function fail(error) {
+        //     alert("An error has occurred: Code = " + error.code);
+        //     console.log("upload error source " + error.source);
+        //     console.log("upload error target " + error.target);
+        //   }
+        //   var options = new FileUploadOptions();
+        //   options.fileKey = "attachment";
+        //   options.fileName = report.imageData.substr(report.imageData.lastIndexOf('/')+1);
+        //   alert(options.fileName);
+        //   options.mimeType="image/jpg";  
+        // //  var headers = { };
+        //   options.headers =null;
+        //   options.chunkedMode = true;
+        //   options.httpMethod="POST";
+        //   alert(JSON.stringify(options));
+        //   var ft = new FileTransfer();
+        //   alert("uri: "+uri);
+        //    window.resolveLocalFileSystemURI(report.imageData, function(fileEntry) {
+        //               alert("file url: "+fileEntry.toURL());
+        //                fileEntry.file(function(fileObj) {
+
+                          
+
+        //         //now use the fileName in your method
+        //           //ft.upload(fileName ,serverURL + '/ajax.php?fname=appuploadspotimage'...);
+        //                  ft.upload(fileObj.localURL, uri, win, fail, options, true);
+        //     });
+
+        //     });       
         }, function (response) {
           // called asynchronously if an error occurs
           // or server returns response with an error status.
@@ -86,14 +148,54 @@ angular.module('starter', ['ionic', 'starter.services', 'ngCordova'])
         });
     };
 
+    function encodeImageUri(imageUri) {
+      var c = document.createElement('canvas');
+      var ctx = c.getContext("2d");
+      var img = new Image();
+      img.onload = function () {
+        c.width = this.width;
+        c.height = this.height;
+        ctx.drawImage(img, 0, 0);
+      };
+      img.src = imageUri;
+      var dataURL = c.toDataURL("image/jpeg");
+      return dataURL;
+    }
+   function b64toBlob(b64Data, contentType, sliceSize) {
+    contentType = contentType || '';
+    sliceSize = sliceSize || 512;
+
+    var byteCharacters = atob(b64Data);
+    var byteArrays = [];
+
+    for (var offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+        var slice = byteCharacters.slice(offset, offset + sliceSize);
+
+        var byteNumbers = new Array(slice.length);
+        for (var i = 0; i < slice.length; i++) {
+            byteNumbers[i] = slice.charCodeAt(i);
+        }
+
+        var byteArray = new Uint8Array(byteNumbers);
+
+        byteArrays.push(byteArray);
+    }
+
+    var blob = new Blob(byteArrays, {type: contentType});
+    return blob;
+}
+
     $scope.getPhoto = function () {
       Camera.getPicture({
         quality: 75,
         targetWidth: 320,
         targetHeight: 320,
-        saveToPhotoAlbum: false
+        saveToPhotoAlbum: false,
+        //destinationType: 0
       }).then(function (imageURI) {
         // console.log(imageData);
+       // alert(imageData);
+
         $scope.report.imageData = imageURI;
         //$scope.report.lastPhoto = imageURI;
       }, function (err) {
